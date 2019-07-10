f8e5ad6df0516b00d4a69b2de6bb06c717807059
Ding CAO <42603285+DwayneCao@users.noreply.github.com>
2019-07-10 22:37:03 +0800

    Add files via upload

---
+++ Tracking by group.m
@@ -0,0 +1,25 @@
+%%
+filePath=dir('Y:\data\20190628\*.mat');
+
+%%
+for ii=1:36
+    filePath=dir('Y:\data\20190628\*.mat');
+    load([filePath(ii).folder,'\',filePath(ii).name]);
+    FileName=[filePath(ii).folder,'\',ImageInfo.FileName,ImageInfo.FileType];
+    ImageInfo.File_id=FileName;
+    ImageInfo.Path=[filePath(1).folder,'\'];
+    clear('filePath');
+    save([ImageInfo.Path, 'modify Trace ', ImageInfo.FileName, '.mat'])
+    clear;
+end
+%%
+
+
+
+for ii=17:19
+   dbstop if error
+   filePath=dir('Y:\data\20190705\*.nd2');
+   Tracking([filePath(ii).folder,'\',filePath(ii).name],'Normalization', 'off');
+end
+
+
