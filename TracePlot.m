38472a4a20b5be4b5dbeb474aaa6bed0fa30b1a3
Ding CAO <42603285+DwayneCao@users.noreply.github.com>
2019-07-11 20:58:47 +0800

    add the start and end number of the plotted trace

--- TracePlot.m
+++ TracePlot.m
@@ -1,4 +1,4 @@
-function [] = TracePlot(Trace_All)
+function [] = TracePlot(Trace_All,StratEndNum)
 
 figure()
 hold on
@@ -8,9 +8,9 @@ box on
 set(gca,'FontSize',16,'LineWidth',1.5)
 LineTextColor=lines(size(Trace_All, 2));
 
-for i = 1:size(Trace_All, 2)
+for i = StratEndNum
     plot(Trace_All{i}(:,2),Trace_All{i}(:,3),'LineWidth',1.5,'color',LineTextColor(i,:))
-    text(Trace_All{i}(floor(end/2),2),Trace_All{i}(floor(end/2),3),num2str(i),'FontWeight','bold','FontSize',16,'color',LineTextColor(i,:))
+    text(Trace_All{i}(end,2),Trace_All{i}(end,3),num2str(i),'FontWeight','bold','FontSize',16,'color',LineTextColor(i,:))
 end
 
 end
