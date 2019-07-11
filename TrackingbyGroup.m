00f3b9947350ffa7adcb683d7d5f84daf62ec76c
Ding CAO <42603285+DwayneCao@users.noreply.github.com>
2019-07-11 20:57:00 +0800

    add comments

--- TrackingbyGroup.m
+++ TrackingbyGroup.m
@@ -1,7 +1,7 @@
-%%
+%% find the path
 filePath=dir('Y:\data\20190628\*.mat');
 
-%%
+%% modify the path parameters for different computer 
 for ii=1:36
     filePath=dir('Y:\data\20190628\*.mat');
     load([filePath(ii).folder,'\',filePath(ii).name]);
@@ -12,10 +12,8 @@ for ii=1:36
     save([ImageInfo.Path, 'modify Trace ', ImageInfo.FileName, '.mat'])
     clear;
 end
-%%
 
-
-
+%% tracking by group
 for ii=17:19
    dbstop if error
    filePath=dir('Y:\data\20190705\*.nd2');
