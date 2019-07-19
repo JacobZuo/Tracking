ff74ffc88c749ad821d5a2117e531dd8c6fa6125
Jacob Zuo <zwlong@live.com>
2019-07-19 11:22:45 +0800

    Remove nd2reader file.

--- ND2Close.m
+++
@@ -1,9 +0,0 @@
-function [] = ND2Close(FilePointer)
-
-    if exist('FilePointer','var')
-        calllib('Nd2ReadSdk', 'Lim_FileClose', FilePointer);
-        clear('FilePointer')
-    else
-    end
-end
-
